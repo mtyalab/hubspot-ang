8ed18396d11f95c0a3bba09833613a9ed9aefed0
Krista Mutyaba <mutyabakrista2016@gmail.com>
2020-12-14 03:48:00 +0300

    Upgrade home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {ProfileService} from '../../services/profile.service';
+import {Profile} from '../../models/profile';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  userFormData: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private profileService: ProfileService) { }
 
   ngOnInit() {
+    this.userFormData = this.formBuilder.group({
+       firstName: '',
+       lastName: '',
+       phoneNumber: '',
+      email: '',
+      password: '',
+      terms: false
+    });
   }
+
+   onSubmit() {
+      this.profileService.signUp(this.userFormData.value).subscribe(data => {
+        console.log('User form data:' + data);
+        },
+        error => {
+        console.log(error);
+        });
+   }
 
 }
