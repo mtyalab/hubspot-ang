41257049f5386bd04f311ba635e14c536c513689
Krista Mutyaba <mutyabakrista2016@gmail.com>
2020-12-14 03:26:28 +0300

    import Reactive forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import {appRoutingModule} from './app-routing';
 import { SuccessComponent } from './components/success/success.component';
+import { AppInputComponent } from './components/modules/app-input/app-input.component';
+import { AppButtonComponent } from './components/modules/app-button/app-button.component';
+import { AppInputLabelComponent } from './components/modules/app-input-label/app-input-label.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { AppInputCheckboxComponent } from './components/modules/app-input-checkbox/app-input-checkbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SuccessComponent
+    SuccessComponent,
+    AppInputComponent,
+    AppButtonComponent,
+    AppInputLabelComponent,
+    AppInputCheckboxComponent
   ],
   imports: [
     BrowserModule,
-    appRoutingModule
+    appRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
